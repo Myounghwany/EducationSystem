be2538d1802eb85a064826646bcfe3a19ea9b30e
DESKTOP-SHSSN04\bit <lauren8480@gmail.com>
2018-04-20 11:23:27 +0900

    no message

--- src/com/es/handler/DefaultHandler.java
+++ src/com/es/handler/DefaultHandler.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.es.db.SqlMapClient;
-
 @Controller
 public class DefaultHandler {
 
